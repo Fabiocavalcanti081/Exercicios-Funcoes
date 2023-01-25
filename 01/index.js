5d4cf6880b44ede9c00c71f547077824eedf2f20
Fábio Cavalcanti <fabiocavalcanti.mkt@gmail.com>
2023-01-25 14:45:44 -0300

    Exercício 01 Sistema de correção de provas Finalizado

--- 01/index.js
+++ 01/index.js
@@ -1,33 +1,33 @@
+const prova = {
+    aluno: "João",
+    materia: "Português",
+    valor: 10,
+    questoes: [
+        {
+            resposta: "a",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        },
+        {
+            resposta: "e",
+            correta: "b"
+        },
+        {
+            resposta: "b",
+            correta: "b"
+        },
+        {
+            resposta: "c",
+            correta: "c"
+        }
+    ]
+};
 function corrigirProva(prova) {
     let nota = 0;
     let questao = 0;
-    prova = {
-        aluno: "João",
-        materia: "Português",
-        valor: 10,
-        questoes: [
-            {
-                resposta: "a",
-                correta: "b"
-            },
-            {
-                resposta: "c",
-                correta: "c"
-            },
-            {
-                resposta: "e",
-                correta: "b"
-            },
-            {
-                resposta: "b",
-                correta: "b"
-            },
-            {
-                resposta: "c",
-                correta: "c"
-            }
-        ]
-    };
 
     for (const item of prova.questoes) {
         if (item.resposta === item.correta) {
@@ -39,4 +39,4 @@ function corrigirProva(prova) {
     console.log(`O aluno(a) ${prova.aluno} acertou ${questao} questões e obteve nota ${nota}`);
 }
 
-corrigirProva();
+corrigirProva(prova);
